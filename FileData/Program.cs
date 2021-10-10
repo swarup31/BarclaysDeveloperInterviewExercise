1bf4641ef8635dec7867611908d5f2c09f882e6a
Swarup jadhav <swarjadhav@gmail.com>
2021-10-10 16:16:55 +0530

    Intital Push

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -12,30 +12,6 @@ namespace FileData
 
             try
             {
-                //FileDetails fileDetails = new FileDetails();
-                //if (args.Length > 0)
-                //{
-                //    #region Task1
-                //    if (args[0] == "-v")
-                //    {
-                //        Console.WriteLine(fileDetails.Version(args[args.Length - 1]));
-                //    }
-                //    #endregion
-
-                //    #region Task2
-                //    if (args[0] == "-v" || args[0] == "--v" || args[0] == "/v" || args[0] == "--version")
-                //    {
-                //        Console.WriteLine(fileDetails.Version(args[args.Length - 1]));
-                //    }
-                //    else if (args[0] == "-s" || args[0] == "--s" || args[0] == "/s" || args[0] == "--size")
-                //    {
-                //        Console.WriteLine(fileDetails.Size(args[args.Length - 1]));
-                //    }
-                //    #endregion
-
-                //}
-
-
                 IReadFileData fileData = new ReadFileData();
                 fileData.ReadFile(args);
 
